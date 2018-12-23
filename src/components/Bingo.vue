835f3bce720928ee4b22d9edf8077a6b92886ffe
Gazder Bence <bencegazder@gmail.com>
2018-12-23 16:18:13 +0100

    Add proper answer display

--- src/components/Bingo.vue
+++ src/components/Bingo.vue
@@ -1,10 +1,12 @@
 <template>
     <div id="content">
-        <table>
+        <table v-if="answers.length > 0">
             <tbody>
                 <tr v-bind:key="y" v-for="y in tableHeight">
-                    <td  class="bingo" v-bind:key="x" v-for="x in tableWidth">
-                        {{ answers[(y - 1)*tableHeight + x] }}
+                    <td @click="answerClick(answers[(y - 1)*tableHeight + (x-1)])" class="bingo"
+                        :class="isActive(answers[(y - 1)*tableHeight + (x-1)]) ? 'active' : ''" v-bind:key="x"
+                        v-for="x in tableWidth">
+                        {{ answers[(y - 1)*tableHeight + (x-1)].answer }}
                     </td>
                 </tr>
 
@@ -17,17 +19,26 @@
     import BingoElements from '../assets/items'
 
     export default {
+        mounted() {
+            const bingoAnswers = BingoElements.answers
+            this.answers = this._.shuffle(bingoAnswers).slice(0,25).map((element) => {
+                return { count: 0, answer: element }
+            })
+        },
         data() {
           return {
               tableWidth: 5,
-              tableHeight: 5
+              tableHeight: 5,
+              answers: [],
           }
         },
-        computed: {
-            answers() {
-                const bingoAnswers = BingoElements.answers
-                return this._.shuffle(bingoAnswers).slice(0,26)
-            }
+        methods: {
+          isActive(element) {
+              return element.count > 0
+          },
+          answerClick(element) {
+              element.count++
+          }
         }
     }
 </script>
