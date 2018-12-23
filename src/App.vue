f125265e27e91e522ed4f3030da682c16f2ba99a
Gazder Bence <bencegazder@gmail.com>
2018-12-23 16:27:09 +0100

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
+    <AppHeader></AppHeader>
     <router-view/>
   </div>
 </template>
 
+<script>
+  import AppHeader from './components/AppHeader'
 
+  export default {
+    components: {
+      AppHeader
+    }
+  }
+</script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <div id="header" style="min-width: 1182px;">
+        <p style="font-size: 22px; padding-top: 14px; margin-left: auto;">Ünnepi Bingo az Ünnepi Ebéd mellé</p>
+    </div>
+</template>
