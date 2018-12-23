a3eedc412fe50bf776f763fc074eadd94de53036
Gazder Bence <bencegazder@gmail.com>
2018-12-24 00:45:42 +0100

    Add google analytics code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-import VueAnalytics from 'vue-analytics'
-
 import './assets/main.css'
 
 import Vue from 'vue'
@@ -15,13 +13,6 @@ Vue.use(VueLodash)
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
-
-if(process.env.NODE_ENV === 'production') {
-  Vue.use(VueAnalytics, {
-    id: 'UA-112064201-3',
-    checkDuplicatedScript: true
-  })
-}
 
 new Vue({
   router,
