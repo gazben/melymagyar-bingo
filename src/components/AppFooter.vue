b0f3f2e53af9327fdcba0a46af5bb7341f3c8259
Gazder Bence <bencegazder@gmail.com>
2021-12-23 23:58:40 +0100

    Update footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,13 +6,14 @@
                     <p class="mb-0"><strong>Made with: love && zsold</strong></p>
                     <p>
                         Az oldal az eredeti unnepiebed.com alapján készült, ami
-                        <a href="https://github.com/sedthh" target="_blank">@sedthh </a> munkája,
+                        <a href="https://github.com/sedthh" target="_blank"><strong>@sedthh</strong></a> munkája,
                         de a készítők nincsenek kapcsolatban egymással!
                     </p>
                 </div>
                 <div class="col-md-6">
                     <p class="mb-0">Köszönöm a segítséget a CSS-el. Most mobilon lista jelenik meg táblázat helyett.</p>
-                    <small><a href="https://github.com/gazben/melymagyar-bingo" target="_blank">Github repo</a></small>
+                    <p class="mb-0"><a href="https://github.com/gazben/melymagyar-bingo" target="_blank"><strong>Github repo</strong></a></p>
+                    <p><a href="https://coub.com/view/2yzkpu" target="_blank"><strong>Magyarország előre megy, nem hátra!</strong></a></p>
                 </div>
             </div>
         </div>
@@ -21,7 +22,7 @@
 
 <style>
 footer {
-  background-color: #790000;
+  background-color: rgba(121, 0, 0, 0.99);
   color: #fff;
   padding: 20px 0;
   text-align: center;
